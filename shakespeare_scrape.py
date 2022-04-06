c58a824c4d4a64973c87f31d85b637cbdd1f0836
Samantha Ryan-Lee <sryanlee@umich.edu>
2022-04-06 03:15:16 -0400

    first training example created, transfer to greaklakes

--- shakespeare_scrape.py
+++ shakespeare_scrape.py
@@ -3,11 +3,13 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 warnings.filterwarnings("ignore")
 
 BASE_URL = 'https://github.com/tokestermw/tensorflow-shakespeare/tree/master/data/shakespeare/sparknotes/merged/'
-shake_html_path = Path("data\shakespeare\shakespeare_response.html")
+data_path = "data/shakespeare/"
+shake_html_path = Path(data_path+"shakespeare_response.html")
 
 if shake_html_path.exists():
     print("USING CACHE")
@@ -24,7 +26,7 @@ results = soup.find(id="repo-content-pjax-container")
 a_class = "js-navigation-open Link--primary"
 a_elements = results.find_all("a", class_=a_class)
 
-print('PROCESSING DATA')
+print('COLLECTING DATA')
 shakespeare_plays = []
 for a_tag in a_elements:
     link_text = a_tag.text
@@ -41,8 +43,9 @@ for a_tag in a_elements:
 
     shakespeare_plays.append(df)
 
-print(f'Number of plays collected and processed: {len(shakespeare_plays)}')
+print(f'Number of tables collected: {len(shakespeare_plays)}')
 
+print('PROCESSING DATA')
 shake_df = pd.concat(shakespeare_plays, axis=0)
 shake_original = shake_df[shake_df['version'] == 'original'].copy().rename(columns={'text': 'original_text'})
 shake_modern = shake_df[shake_df['version'] == 'modern'].copy().rename(columns={'text': 'modern_text'})
@@ -51,6 +54,40 @@ shake_aligned = shake_original.merge(
     ).drop(columns=['version_x', 'title_y', 'version_y', 'line_id_y']
     ).rename(columns={'title_x': 'title', 'line_id_x': 'line'})
 shake_aligned = shake_aligned[['play_line_id', 'title', 'line', 'original_text', 'modern_text']]
+
+print("CLEANING DATA")
+shake_aligned['original_text_cleaned'] = shake_aligned.original_text.str.lower().str.strip()
+shake_aligned['modern_text_cleaned'] = shake_aligned.modern_text.str.lower().str.strip()
+# remove special characters
+shake_aligned.replace(to_replace={
+    'original_text_cleaned': r"[^A-Za-z\s0-9]",
+    'modern_text_cleaned': r"[^A-Za-z\s0-9]",
+}, value=" ", regex=True, inplace=True)
+# fix any added whitespaces from first replace
+shake_aligned.replace(to_replace={
+    'original_text_cleaned': r"\s\s+",
+    'modern_text_cleaned': r"\s\s+",
+}, value=" ", regex=True, inplace=True)
+
+shake_aligned['combined_text_clean'] = shake_aligned.modern_text_cleaned.str.strip() + " [SEP] " + shake_aligned.original_text_cleaned.str.strip()
 print(f'DATA SAMPLE\n{shake_aligned.head()}')
 
-shake_aligned.to_csv("data/shakespeare_translated.csv", index=False, encoding="utf-8")
+
+print(f'CREATING TRAIN, DEV, TEST DATA SPLITS')
+train, all_test = train_test_split(shake_aligned.index, train_size=0.8)
+train_df = shake_aligned.iloc[train].copy().reset_index(drop=True)
+all_test_df = shake_aligned.iloc[all_test].copy().reset_index(drop=True)
+print(f'Number of training rows: {train_df.shape[0]}')
+print(f'Number of dev+test rows: {all_test_df.shape[0]}')
+
+dev, test = train_test_split(all_test_df.index, train_size=0.5)
+dev_df = all_test_df.iloc[dev].copy().reset_index(drop=True)
+test_df = all_test_df.iloc[test].copy().reset_index(drop=True)
+print(f'Number of dev rows: {dev_df.shape[0]}')
+print(f'Number of test rows: {test_df.shape[0]}')
+
+print('WRITING DATA FILES')
+shake_aligned.to_csv(data_path+"shakespeare_translated.csv", index=False)
+train_df.to_csv(data_path+"train.csv", index=False)
+dev_df.to_csv(data_path+"dev.csv", index=False)
+test_df.to_csv(data_path+"test.csv", index=False)
